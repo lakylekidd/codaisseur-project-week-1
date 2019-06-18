70b1a1b83b934ff0d5220e8749e0dd525ac802c9
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-18 14:17:50 +0200

    add defauld state setImgUrl() and start render()

--- src/features/game-guess-bread-by-image/components/Game1Contener.jsx
+++ src/features/game-guess-bread-by-image/components/Game1Contener.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Game1Contener extends Component {
+  state = {
+    guessDog: {
+      id: 1, //num
+      name: 'doggo',
+      imgs: ['url 1', 'url 2'],
+      img: 'url to loding img'
+    }
+  }
+
+  setImgUrl = (imgs = []) => {
+    const r = Math.floor(Math.random() * imgs.length)
+    this.state.guessDog.img = this.state.guessDog.imgs[r]
+  }
+
+  componentDidMount() {
+    this.setImgUrl()
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className='game1'>
+        <image scr={this.state.guessDog.img} />
+        <div className='guessDogs'> {/* use flexbox */} 
+          <div style={`order = ${Math.floor(Math.random() * 1000)}`}></div>
+        </div>
       </div>
     )
   }
