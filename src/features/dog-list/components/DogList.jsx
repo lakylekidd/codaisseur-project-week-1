a869778a5672c7d23099156f5f1f8e57b62f3c23
Billy Vlachos <williamcaminiti@live.com>
2019-06-18 16:56:26 +0200

    Fixing styling of Dog List

--- src/features/dog-list/components/DogList.jsx
+++ src/features/dog-list/components/DogList.jsx
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom'
 export default function DogList(props) {
     return (
         <div className="wrapper">
+            <h1>List of Breeds</h1>
             {
                 props.dogs &&
                 <ul>
                     {
-                        props.dogs.map(dog => 
-                        <li key={dog}><Link to={`/breeds/${dog}`}>{dog}</Link></li>
+                        props.dogs.map(dog =>
+                            <li key={dog}><Link to={`/breeds/${dog}`}>{dog}</Link></li>
                         )
                     }
                 </ul>
