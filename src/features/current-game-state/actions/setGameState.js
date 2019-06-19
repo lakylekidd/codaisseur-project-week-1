ef256e496f34c26aebe5e85bc95c57ef5ac9a594
Billy Vlachos <williamcaminiti@live.com>
2019-06-19 19:50:24 +0200

    Implemented game state reducer

--- src/features/current-game-state/actions/setGameState.js
+++ src/features/current-game-state/actions/setGameState.js
@@ -1,12 +1,16 @@
 export const SET_CURRENT_GAME_STATE = 'SET_CURRENT_GAME_STATE';
 
 // Define the available global game states
-export const STATES = 'IDLE' | 'START' | 'RUNNING' | 'GAME_OVER';
+export const IDLE_STATE = 'IDLE';
+export const START_STATE = 'START';
+export const RUNNING_STATE = 'RUNNING';
+export const GAME_OVER_SATE = 'GAME_OVER'
 
 /**
- * Action that sets the current game state
+ * Action that sets the current game state with default 
+ * state idle and game id null
  */
-export function setGameState(state, gameId = null) {
+export function setGameState(state = IDLE_STATE, gameId = null) {
     return {
         type: SET_CURRENT_GAME_STATE,
         payload: {
--- src/features/current-game-state/game-state.reducer.js
+++ src/features/current-game-state/game-state.reducer.js
@@ -1,12 +1,52 @@
-//import { SET_BREEDS_ARRAY } from './actions/getBreedsArray';
+import {
+    SET_CURRENT_GAME_STATE,
+    IDLE_STATE,     // No game is currently running.
+    START_STATE,    // Game welcome page showing
+    RUNNING_STATE,  // Game logic running
+    GAME_OVER_SATE  // Game over page showing with stats
 
+} from './actions/setGameState';
+
+// Define the initial state
 const initialState = {
     gameId: null,
     state: 'somestate'
 }
 
-export default (state = {}, action = {}) => {
+export default (state = initialState, action = {}) => {
     switch (action.type) {
+        case SET_CURRENT_GAME_STATE:
+            return setCurrentState(state, action);
+        default:
+            return state;
+    }
+}
+
+/**
+ * Function that determines how the current 
+ * state should be changed
+ * @param {The current state} state 
+ * @param {The requested action} action 
+ */
+function setCurrentState(state, action) {
+    // Switch through requested state
+    switch (action.payload.state) {
+        case IDLE_STATE:
+            return initialState;
+        // For every state other than idle
+        // Return the new state and game id
+        case START_STATE:
+        case RUNNING_STATE:
+        case GAME_OVER_SATE:
+            // Check if the game id matches current state
+            if (state.gameId === action.payload.gameId) {
+                // If they match return the new payload
+                return {
+                    ...action.payload
+                }
+            }
+            // Otherwise return the initial state
+            return initialState;
         default:
             return state;
     }
