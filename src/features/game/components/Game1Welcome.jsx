a7a07480248bd9151eebc293fbda726775b2c422
Billy Vlachos <williamcaminiti@live.com>
2019-06-19 20:32:54 +0200

    Implemented welcome page without any logic

--- src/features/game/components/Game1Welcome.jsx
+++ src/features/game/components/Game1Welcome.jsx
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Game1Welcome.css';
 
 export default class Game1Welcome extends Component {
     render() {
         return (
-            <div>
-                welcome billy!!!
+            <div className="game-welcome-page">
+                <h1>Welcome to Game 1</h1>
+                <p>
+                    This mini-game is designed to test your skills
+                    in recognizing all of the most famous dog breeds
+                    from around the world
+                </p>
+                <h2>Instructions:</h2>
+                <ol>
+                    <li>Click 'Start Game'</li>
+                    <li>Choose the right breed based on the displayed image.</li>
+                    <li>You can have a maximum of <b>n</b> wrong answers</li>
+                    <li>Have fun!</li>
+                </ol>
+                <button>Start Game</button>
             </div>
         )
     }
