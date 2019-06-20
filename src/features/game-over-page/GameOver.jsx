668782b695f3862f4b640c6df937dbef1575ee43
Billy Vlachos <williamcaminiti@live.com>
2019-06-20 11:48:16 +0200

    Added game over page and implemented some basic css

---
+++ src/features/game-over-page/GameOver.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import {
+    setGameState,
+    IDLE_STATE,     // No game is currently running.
+    START_STATE,    // Game welcome page showing
+} from './../current-game-state/actions/setGameState';
+import ExitGameButton from './../game/components/ExitGameButton';
+import './GameOver.css';
+
+/**
+ * Page that displays as soon as any game is over
+ */
+class GameOver extends Component {
+    /**
+     * Set the game state to start
+     */
+    restartGame = () => {
+        this.props.setGameState(START_STATE, this.props.currentGameState.gameId);
+    }
+
+    render() {
+        // Retrieve the results from the game
+        const results = {
+            total: 24,
+            wrong: 6,
+            correct: 18,
+            score: 0.28
+        };
+
+        return (
+            <div className="game-over-page">
+                <h1>Game Over</h1>
+                <p>
+                    <b>Sorry!</b> You scored {Math.floor(results.score * 100)}% in the game.
+                    You can retry again or exit the game.
+                </p>
+                <button className="game-restart" onClick={this.restartGame} title="Click to re-start the game!">Restart Game</button>
+                <ExitGameButton />
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = {
+    setGameState
+}
+const mapStateToProps = (reduxState) => {
+    return {
+        currentGameState: reduxState.currentGameState
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameOver);
