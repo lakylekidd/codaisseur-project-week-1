585cbbad56da16c59cc2bd1cd8c0b983991235b1
Billy Vlachos <williamcaminiti@live.com>
2019-06-21 11:36:34 +0200

    Created a new game statistics screen

---
+++ src/features/game-stats/GameStatsScreen.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GameStatsScreen extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
