b90a235809125c6916202accbc979a38f598aa2d
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-21 14:27:02 +0200

    Added properties and function to reducer

--- src/features/game/answerReducer.js
+++ src/features/game/answerReducer.js
@@ -25,6 +25,15 @@ export default (state = initialState, action = {}) => {
             // Answers retrieved
             const percentage = getPercentage(newState.answers);
             const mistakes = getWrongAnswers(newState.answers);
+            const consecutiveCorrect = tallyCorrect(newState.answers);
+            let level = newState.difficultyLevel;
+            let retrieve = newState.guessBreedsToRetrieve
+            if (consecutiveCorrect >= 5 && level < 2) {
+                level++;
+                retrieve = retrieve +3;
+            }
+            
+            
             //if x mistakes are made, it's game over
             if (mistakes >= state.allowedWrongAnswers) {
                 return {
@@ -32,7 +41,9 @@ export default (state = initialState, action = {}) => {
                     score: percentage / newState.answers.length * 100,
                     gameOver: true,
                     allowedWrongAnswers: 3,
-                    mistakes: mistakes
+                    mistakes: mistakes,
+                    difficultyLevel: level,
+                    guessBreedsToRetrieve: retrieve
                 }
             } else
                 // Return the new state
@@ -41,7 +52,9 @@ export default (state = initialState, action = {}) => {
                     score: percentage / newState.answers.length * 100,
                     gameOver: false,
                     allowedWrongAnswers: 3,
-                    mistakes: mistakes
+                    mistakes: mistakes,
+                    difficultyLevel: level,
+                    guessBreedsToRetrieve: retrieve
                 }
         case CLEAR_CURRENT_GAME_DATA:
             return initialState;
@@ -65,9 +78,7 @@ function getPercentage(arrayOfAnswers) {
 }
 
 function getWrongAnswers(arrayOfAnswers) {
-    if (!arrayOfAnswers.length) {
-        return 0
-    }
+    
     const incorrectAnswers = arrayOfAnswers.reduce((count, currentAnswer) => {
         if (!currentAnswer.correct) {
             count++
@@ -76,4 +87,22 @@ function getWrongAnswers(arrayOfAnswers) {
     }, 0)
 
     return incorrectAnswers;
+}
+
+function tallyCorrect(arrayOfAnswers) {
+    if (!arrayOfAnswers.length) {
+        return 0
+    }
+    const correctAnswers = arrayOfAnswers.reduce((count, currentAnswer) => {
+        if (!currentAnswer.correct) {
+            count = 0
+        } else if (currentAnswer.correct && count.length < 5) {
+            count++
+        } else if (currentAnswer.correct && count.length >= 5) {
+            count =0
+        }
+        return count
+    }, 0)
+    console.log("WHAT IS TALLY??", correctAnswers)
+    return correctAnswers;
 }
